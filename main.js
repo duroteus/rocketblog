dc1c118f8481436c1858cae6a837e83ea1c6f0b3
Gustavo <gustavo.kjr@gmail.com>
2022-04-09 17:50:28 -0300

    responsive to desktop

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
 const swiper = new Swiper('.swiper', {
   slidesPerView: 1,
   pagination: {
