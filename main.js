1c231fd643b5fcf091ee312914af625e214047e6
Gustavo <gustavo.kjr@gmail.com>
2022-04-12 14:14:55 -0300

    responsive to desktop

--- main.js
+++ main.js
@@ -29,7 +29,12 @@ const swiper = new Swiper('.swiper', {
   loop: true,
 
   breakpoints: {
-    412: {
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+
+    768: {
       slidesPerView: 3,
       autoplay: false,
       spaceBetween: 30,
